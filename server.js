fb756b6fe0e58a37215555a0aa91e6b2bf9e6811
muhsalaaio <muhsalaa@armillary.io>
2019-11-27 11:58:56 +0700

    manually rmv es6

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express    = require("express");
-const app        = express();
+var express    = require("express");
+var app        = express();
 
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 app.use(express.static("./"));
 
